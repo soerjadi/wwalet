a2f668d13f361fd6e25e68b393047fecb0702007
soerja <soerja.610@gmail.com>
2024-09-29 03:02:15 +0700

    fixup typo

--- internal/model/wallet.go
+++ internal/model/wallet.go
@@ -4,7 +4,7 @@ import "time"
 
 type Wallet struct {
 	ID        string    `db:"id" json:"id"`
-	UserID    string    `db"user_id" json:"user_id"`
+	UserID    string    `db:"user_id" json:"user_id"`
 	Balance   int64     `db:"balance" json:"balance"`
 	CreatedAt time.Time `db:"created_at" json:"-"`
 	UpdatedAt time.Time `db:"updated_at" json:"-"`

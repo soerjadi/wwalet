7db8e0e553fd5ac4a1f834d2c22dbaa228e96223
soerja <soerja.610@gmail.com>
2024-09-28 21:24:38 +0700

    refine schema structure

--- script/sql/schema.sql
+++ script/sql/schema.sql
@@ -15,16 +15,26 @@ CREATE TABLE IF NOT EXISTS  wallets (
     user_id varchar(55) not null,
     balance bigint not null,
     created_at timestamp default current_timestamp,
-    updated_at timestamp
+    updated_at timestamp,
+    constraint fk_wallets_users
+        foreign key (user_id)
+        references users (id)
 );
+
+CREATE TYPE transaction_status as enum ('SUCCESS', 'FAILED');
+CREATE TYPE transaction_type as enum ('DEBIT', 'CREDIT');
+CREATE TYPE transaction_category as enum ('topup', 'transfer', 'payment');
 
 CREATE TABLE IF NOT EXISTS transactions (
     id varchar(55) not null primary key,
     user_id varchar(55) not null,
-    status enum('SUCCESS', 'FAILED') not null,
-    type enum('DEBIT', 'CREDIT') not null,
-    category enum('topup', 'transfer', 'payment') not null,
+    status transaction_status not null,
+    type transaction_type not null,
+    category transaction_category not null,
     amount bigint not null,
     remarks text,
-    created_at timestamp default current_timestamp
+    created_at timestamp default current_timestamp,
+    constraint fk_transactions_users
+        foreign key (user_id)
+        references users (id)
 );
